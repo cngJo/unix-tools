76ffa787e22a4a3d4eccac43259bac6e9b93424e
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-02-20 19:07:27 +0100

    Cat: Being able to read a given file

--- src/cat/main.c
+++ src/cat/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("hello, cat!");
+char* read_file(char* filename) {
+    char* buffer;
+    size_t result;
+    long size;
+
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        fputs("File Error", stderr);
+        exit(1);
+    }
+
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    rewind(file);
+    buffer = malloc(size);
+
+    result = fread(buffer, 1, size, file);
+    if (result != size) {
+        fputs("Reading error 2", stderr);
+        exit(3);
+    }
+
+    fclose(file);
+
+    return buffer;
+}
+
+int main(int argc, char** argv) {
+    // the filename we want to print lives in the second argument we get
+    char* filename = argv[1];
+
+    char* file_content = read_file(filename);
+
+    printf("%s\n", file_content);
 
     return 0;
 }
