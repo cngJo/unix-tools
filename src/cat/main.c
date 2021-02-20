6ffa042f8147a5cf83f027e1580c5f74bcc45f5d
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-02-20 19:08:29 +0100

    Cat: add comments

--- src/cat/main.c
+++ src/cat/main.c
@@ -6,24 +6,27 @@ char* read_file(char* filename) {
     size_t result;
     long size;
 
+    // Open the file
     FILE *file = fopen(filename, "r");
-
     if (file == NULL) {
         fputs("File Error", stderr);
         exit(1);
     }
 
+    // determine the length of the file
     fseek(file, 0, SEEK_END);
     size = ftell(file);
     rewind(file);
     buffer = malloc(size);
 
+    // read the file
     result = fread(buffer, 1, size, file);
     if (result != size) {
         fputs("Reading error 2", stderr);
         exit(3);
     }
 
+    // close the file
     fclose(file);
 
     return buffer;
